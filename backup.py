b35903fa8b536311b7b1a2d10efbdf21124321ca
Sam Augustyn <25727738+sam-augustyn@users.noreply.github.com>
2019-04-20 19:26:30 -0400

    Updated to v3, added download feature
    Updated the code to use API version 3
    An authentication method has been added to support only service account authentication
    A download method has been added to download a file and save it to the current working directory

--- backup.py
+++ backup.py
@@ -1,15 +1,69 @@
-#!/usr/bin/env python
-import os
-import datetime
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+import io
 
-directory = ""
-save = ""
+#SCOPES = ['https://www.googleapis.com/auth/drive']
+#SERVICE_ACCOUNT_FILE = 'service.json'
 
-now = datetime.datetime.now()
-date = str(now.month)+str(now.day)+str(now.year-2000)
-os.system("zip -r %s/project%s.zip %s" % (save, date, directory))
 
-'''file_metadata = {'name': 'project%s.zip' % (date)}
-media = MediaFileUpload('%s/project%s' % (save, date),mimetype='application/zip')
-file = drive_service.files().create(body=file_metadata,media_body=media,fields='id').execute()
-print 'File ID: %s' % file.get('id')'''
+def authenticate(apiName, apiVersion, apiScope):
+    ''' Authenticate the user and returns a service object
+    @apiName should be the name of the google api
+    found here: https://developers.google.com/api-client-library/python/apis/
+    @apiVerison should be the version of the api
+    @apiScope should be the scope of the google api, this script uses OAuth2 Service Accounts
+    found here: https://developers.google.com/identity/protocols/googlescopes '''
+
+    #specify service account file (contains service account information)
+    SERVICE_ACCOUNT_FILE = 'service.json'
+    #create a credentials object with the service account file and the specificed scope
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=apiScope)
+    #build the service object
+    service = build(apiName, apiVersion, credentials=credentials)
+    #return the service object
+    return service
+
+def buildFileDictionary(service):
+    ''' Return a dictionary with the format files{name:id}
+    @service should be the service object generated in authenticate '''
+
+    # Call the Drive v3 API
+    results = service.files().list(fields="nextPageToken, files(id, name)").execute()
+    # return an array of files with their associated id
+    return results.get('files', [])
+
+def printAllFiles(fileDictionary):
+    ''' Prints out a list of files from a dictionary
+    @fileDictionary should be a dictionary containing filenames and file ids'''
+
+    #print each file with its id
+    for file in fileDictionary:
+        print "%s : %s" % (file["name"], file["id"])
+
+def downloadFile(fileId, fileName, service):
+    ''' Downloads the file to the current working directory
+    @fileId should be a file id
+    @service should be a service object from the authenticate method'''
+
+    # create a file to write the bytes too
+    fileBuffer = io.FileIO('./%s' % fileName, 'wb')
+    #
+    request = service.files().get_media(fileId=fileId)
+    downloader = MediaIoBaseDownload(fileBuffer, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print "Download %d%%." % int(status.progress() * 100)
+    #convert the buffer to a file
+
+    fileBuffer.close()
+
+def main():
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    service = authenticate('drive', 'v3', SCOPES)
+    files =  buildFileDictionary(service)
+    #printAllFiles(files)
+    downloadFile(files[0]["id"],files[0]["name"], service)
+
+if __name__ == "__main__": main()
